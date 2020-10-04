1c9135dbf0f09b0e5924713e9b16071bcf2e4217
Garvit Anand <you@example.com>
2020-10-04 16:25:10 +0530

    Quick fix

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,20 +1,38 @@
-import app from "firebase/app";
+import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyAm-_scOoGmAbARCXms6askSZQAQ-U_LDM",
-    authDomain: "react-sample-891a0.firebaseapp.com",
-    databaseURL: "https://react-sample-891a0.firebaseio.com",
-    projectId: "react-sample-891a0",
-    storageBucket: "react-sample-891a0.appspot.com",
-    messagingSenderId: "659219665642",
-    appId: "1:659219665642:web:796376c56f60b0b7c73d93",
-    measurementId: "G-VJSBXW9W3H",
+  apiKey: "AIzaSyAm-_scOoGmAbARCXms6askSZQAQ-U_LDM",
+  authDomain: "react-sample-891a0.firebaseapp.com",
+  databaseURL: "https://react-sample-891a0.firebaseio.com",
+  projectId: "react-sample-891a0",
+  storageBucket: "react-sample-891a0.appspot.com",
+  messagingSenderId: "659219665642",
+  appId: "1:659219665642:web:796376c56f60b0b7c73d93",
+  measurementId: "G-VJSBXW9W3H",
 };
 
 class Firebase {
   constructor() {
     app.initializeApp(config);
+
+    this.auth = app.auth();
   }
+
+  // *** Auth API ***
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
